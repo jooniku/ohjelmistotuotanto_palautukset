d1a6a672008a425c10f4307e5c6df1c37d6dddb3
Joona Nikula <nikulajoona@gmail.com>
2023-11-05 21:01:34 +0200

    nhl testikattavuus 100%

---
+++ nhl-statistics-1/src/tests/statistics_service_test.py
@@ -0,0 +1,54 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+
+    def test_service_has_correct_players(self):
+        correct_players = [i.__str__() for i in PlayerReaderStub().get_players()]
+
+        players = [i.__str__() for i in self.stats._players]
+
+        self.assertEqual(correct_players, players)
+
+
+    def test_search_returns_correct(self):
+        name = "Semenko"
+        found = self.stats.search(name)
+
+        self.assertEqual(name, found.name)
+
+    def test_search_returns_none_if_no_player(self):
+        name = "Rölli"
+        found = self.stats.search(name)
+
+        self.assertEqual(None, found)
+    
+    def test_team_returns_correct_team(self):
+        team = "PIT"
+        found = self.stats.team(team)
+
+        self.assertEqual("Lemieux", found[0].name)
+    
+    def test_ranking_works(self):
+        correct = ["Gretzky", "Lemieux", "Yzerman"]
+
+        found = [i.name for i in self.stats.top(2)]
+
+        self.assertEqual(correct, found)
+
